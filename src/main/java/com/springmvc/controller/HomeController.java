cd2c64c18bc229f8381703a6deb64cb02501cbd1
yunseho <sehoho1018@gmail.com>
2026-05-31 16:18:40 +0900

    주석 테스트
    주석 테스트

--- src/main/java/com/springmvc/controller/HomeController.java
+++ src/main/java/com/springmvc/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+	// API 주소 변수
 	@Value("${kakao.javascript-key}")
 	private String js_apikey;
 
